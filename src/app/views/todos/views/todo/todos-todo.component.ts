4c204a7924378e4338c117bef3e134944601912d
C0ZEN <geoffrey.testelin@gmail.com>
2018-12-31 01:08:04 +0100

    feat: loader before loading todo avatar

--- src/app/views/todos/views/todo/todos-todo.component.ts
+++ src/app/views/todos/views/todo/todos-todo.component.ts
@@ -3,6 +3,10 @@ import {
   OnDestroy,
   OnInit
 } from '@angular/core';
+import {
+  NavigationEnd,
+  Router
+} from '@angular/router';
 import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 import { Subscription } from 'rxjs';
 import { TodoInterface } from '../../../../stores/todos/todo.interface';
@@ -16,11 +20,14 @@ import { TodosQuery } from '../../../../stores/todos/todos.query';
 })
 export class TodosTodoComponent implements OnInit, OnDestroy {
   public todo: TodoInterface | undefined;
+  public isAvatarLoaded: boolean = false;
 
   private selectActiveTodo: Subscription | undefined;
+  private routerEvents: Subscription | undefined;
 
   public constructor(
-    private todosQuery: TodosQuery
+    private todosQuery: TodosQuery,
+    private router: Router
   ) {
   }
 
@@ -28,8 +35,17 @@ export class TodosTodoComponent implements OnInit, OnDestroy {
     this.selectActiveTodo = this.todosQuery.selectActive().subscribe((activeTodo: TodoInterface | undefined) => {
       this.todo = activeTodo;
     });
+    this.routerEvents = this.router.events.subscribe(routerEvent => {
+      if (routerEvent instanceof NavigationEnd) {
+        this.isAvatarLoaded = false;
+      }
+    });
   }
 
   public ngOnDestroy(): void {
+  }
+
+  public onAvatarLoad(): void {
+    this.isAvatarLoaded = true;
   }
 }
