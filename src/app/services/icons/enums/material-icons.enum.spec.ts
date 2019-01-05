393c972e95092a3d5c2dbd7add11e7e52cec8873
C0ZEN <geoffrey.testelin@gmail.com>
2019-01-05 15:49:29 +0100

    test: add 100% coverage for material icons enum

---
+++ src/app/services/icons/enums/material-icons.enum.spec.ts
@@ -0,0 +1,47 @@
+import { MaterialIconsEnum } from './material-icons.enum';
+
+describe('MaterialIconsEnum:', () => {
+  it('should have a member "SPEED" which equals "speed"', () => {
+    expect(MaterialIconsEnum.SPEED).toBe('speed');
+  });
+
+  it('should have a member "DOCUMENTS_FOLDER" which equals "documents-folder"', () => {
+    expect(MaterialIconsEnum.DOCUMENTS_FOLDER).toBe('documents-folder');
+  });
+
+  it('should have a member "WEB_DESIGN" which equals "web-design"', () => {
+    expect(MaterialIconsEnum.WEB_DESIGN).toBe('web-design');
+  });
+
+  it('should have a member "CREATE_ICON" which equals "create-icon"', () => {
+    expect(MaterialIconsEnum.CREATE_ICON).toBe('create-icon');
+  });
+
+  it('should have a member "RULES" which equals "rules"', () => {
+    expect(MaterialIconsEnum.RULES).toBe('rules');
+  });
+
+  it('should have a member "CUBE" which equals "cube"', () => {
+    expect(MaterialIconsEnum.CUBE).toBe('cube');
+  });
+
+  it('should have a member "ADD_CIRCLE" which equals "add-circle"', () => {
+    expect(MaterialIconsEnum.ADD_CIRCLE).toBe('add-circle');
+  });
+
+  it('should have a member "INDEX" which equals "index"', () => {
+    expect(MaterialIconsEnum.INDEX).toBe('index');
+  });
+
+  it('should have a member "CHECKLIST" which equals "checklist"', () => {
+    expect(MaterialIconsEnum.CHECKLIST).toBe('checklist');
+  });
+
+  it('should have a member "WHITE_ADD" which equals "white-add"', () => {
+    expect(MaterialIconsEnum.WHITE_ADD).toBe('white-add');
+  });
+
+  it('should have a member "WHITE_ADD_CIRCLE" which equals "white-add-circle"', () => {
+    expect(MaterialIconsEnum.WHITE_ADD_CIRCLE).toBe('white-add-circle');
+  });
+});
